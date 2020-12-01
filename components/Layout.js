90fab3e80894362443973e1209567cc18aa14dfe
Timofey Chunarev <timofei.chunarev@gmail.com>
2020-12-01 13:02:57 +0700

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import styles from './layout.module.css'
 import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/Link'
+import Link from 'next/link'
 
 
 const name = "Timofey Chunarev"
