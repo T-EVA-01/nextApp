3bdf419babd125cb592ff8cef58c7a59661d9e5c
Timofey Chunarev <timofei.chunarev@gmail.com>
2020-12-15 22:44:03 +0700

    feat(components): create the Article component

---
+++ components/Article/index.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+const Article = styled.article`
+    
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+        font-size: 30px;
+        font-weight: bold;
+        padding: 0;
+        /* margin-right: 30px; */
+        margin-bottom: 30px;
+        align-self: center;
+    }
+
+
+    p {
+        margin-top: 0;
+        margin-bottom: 15px;
+    }
+
+    
+    p:last-child {
+        margin-bottom: 0;
+    }
+    
+`
+
+const Index = ({className, articleTitle, articleText}) => {
+
+    console.log(articleText)
+
+    return (
+        <Article className={className}>
+            <h1>{articleTitle}</h1>
+            {articleText && <div>{
+                articleText.map(item => {
+                    return <p>{item}</p>
+                })
+            }</div>}
+        </Article>
+    )
+} 
+
+export default Index 
